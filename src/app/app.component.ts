c52f4b10322f7ea8e93a9e155919f75cbb0af2ed
Joao Fonseca <jmgf_13@hotmail.com>
2023-03-30 06:08:42 +0100

    Add editor to project & implement design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from "@angular/core";
+
+declare var pyscript: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'easy-programming';
+export class AppComponent implements AfterViewInit {
+  some = 'for i in range(8):\n\t\tprint(i)';
+
+  ngAfterViewInit(): void {
+    const a = document.getElementsByClassName("py-terminal");
+    console.log(a);
+    console.log(pyscript.interpreter);
+    console.log(pyscript.runtime.stdio._listeners[0]);
+  }
+
+  sortInPython(data: any) {
+    console.log(data);
+
+    // js_sorted = pyscript.interpreter.globals.get('sorted') //grab python's 'sorted' function
+    // const sorted_data = js_sorted(data) //apply the function to the 'data' argument
+    // for (const item of sorted_data){
+    //     console.log(item)
+    // }
+  }
 }
