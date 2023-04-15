809393781f8062dd4a8f0ba743d49c3101938a62
Joao Fonseca <jmgf_13@hotmail.com>
2023-04-15 17:57:17 +0100

    Add pseudocode editor to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,20 @@ interface ElementWithInnerText extends Element {
 })
 export class AppComponent {
   codeStoredInDatabase = "for i in range(8):\n\t\tprint(i)";
+  pseudocodeStoredInDatabase: string = "";
+  pseudocodeOptions = {
+    lineNumbers: true,
+    theme: "default",
+    mode: "python",
+    lineWrapping: true,
+    extraKeys: {
+      "Ctrl-Q": function (cm: any) {
+        cm.foldCode(cm.getCursor());
+      },
+    },
+    foldGutter: true,
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+  };
 
   execute(): void {
     const repl = document.getElementsByClassName("cm-content");
@@ -24,5 +38,10 @@ export class AppComponent {
     const repl = document.getElementsByClassName("cm-content");
     const code = (repl[0] as ElementWithInnerText).innerText;
     this.codeStoredInDatabase = code.trim();
+  }
+
+  handleChange(event: string) {
+    // console.log("this.content", this.content);
+    console.log(event);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { MatTabsModule } from "@angular/material/tabs";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CodemirrorModule } from "@ctrl/ngx-codemirror";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatTabsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatTabsModule, FormsModule, CodemirrorModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
