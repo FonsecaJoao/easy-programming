77ce060749a09e8e0bb8633d3f0fb61bc9ee5a7d
DuarteSilva22 <duartesilva22@outlook.com>
2023-06-11 19:39:18 +0100

    Create new users and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,14 @@ import { SignupComponent } from "./auth/signup/signup.component";
 import { EducationAreaComponent } from "./components/education-area/education-area.component";
 import { FlowchartComponent } from './components/flowchart/flowchart.component';
 import { HeaderComponent } from './header/header.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from "./auth/auth-interceptor";
+
 
 @NgModule({
   declarations: [AppComponent, FlowchartComponent, HeaderComponent, LoginComponent, SignupComponent, EducationAreaComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, CodemirrorModule, DiagramModule, SymbolPaletteModule, AngularMaterialModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, CodemirrorModule, DiagramModule, SymbolPaletteModule, AngularMaterialModule, AppRoutingModule, HttpClientModule],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
